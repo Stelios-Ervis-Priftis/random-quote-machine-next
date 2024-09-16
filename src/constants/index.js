f00e7eddba6809fcaf57b917da1a2166f7ba530a
Stelios Ervis Prifits <steliospriftis@gmail.com>
2024-09-16 13:39:53 +0200

    fix(color): create and export option object with dark luminosity

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,7 @@ export const API_BASE_URL = 'https://api.api-ninjas.com/v1/quotes';
 export const LOCAL_STORAGE_QUOTES_KEY = 'myQuote';
 export const COUNTDOWN_DURATION = 5;
 export const ERROR_COUNTDOWN_DURATION = 3;
+export const COLOR_LUMINOSITY = { luminosity: 'dark' };
 
 // Messages
 export const API_ERROR_MESSAGE =
