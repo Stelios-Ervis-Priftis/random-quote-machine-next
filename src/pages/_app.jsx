25591cd83da75f231d153f09be62d6a11fa45b04
Stelios Ervis Prifits <steliospriftis@gmail.com>
2024-10-01 15:43:12 +0200

    refactor(react query): utilise react query

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,19 @@
-import '@/styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import '@/styles/globals.css';
+
+const queryClient = new QueryClient();
+
 export default function App({ Component }) {
-  return <Component />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 App.propTypes = {
