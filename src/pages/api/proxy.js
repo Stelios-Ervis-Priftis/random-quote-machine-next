c91f09c0f34113ec305bc0ba2711200ca1fe91ff
Stelios Ervis Prifits <steliospriftis@gmail.com>
2024-10-01 13:30:58 +0200

    refactor(proxy): return the only obj from the array response

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -12,7 +12,7 @@ const config = {
 export default async function handler(req, res) {
   try {
     const response = await axios.request(config);
-    res.status(200).json(response.data);
+    res.status(200).json(response.data[0]);
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({
