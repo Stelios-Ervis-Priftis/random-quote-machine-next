ce43dd6a5f8a233926631c5c6b69e0d408ccfeef
Stelios Ervis Prifits <steliospriftis@gmail.com>
2024-10-02 17:14:53 +0200

    refactor(proxy): clean up and refactor proxy

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -1,21 +1,20 @@
 import { API_BASE_URL } from '@/constants/index';
 import axios from 'axios';
 
-const config = {
-  method: 'get',
-  url: API_BASE_URL,
-  headers: {
-    'X-Api-Key': process.env.API_NINJAS_KEY,
-  },
-};
+const handler = async (request, response) => {
+  const apiConfig = {
+    method: 'get',
+    url: API_BASE_URL,
+    headers: {
+      'X-Api-Key': process.env.API_NINJAS_KEY,
+    },
+  };
 
-const handler = async (req, res) => {
   try {
-    const response = await axios.request(config);
-    res.status(200).json(response.data[0]);
+    const { data } = await axios.request(apiConfig);
+    response.status(200).json(data[0]);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({
+    response.status(500).json({
       error: 'Failed to fetch data',
       details: error.message,
     });
