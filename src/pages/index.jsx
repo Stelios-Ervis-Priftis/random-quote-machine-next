64db90421f046825c7922184828313db41405d58
Stelios Ervis Prifits <steliospriftis@gmail.com>
2024-10-02 10:50:14 +0200

    refactor(app index.js): remove unnecessary cleand up from refactor, utilise react query
    No used any more, with many others as well

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,101 +1,42 @@
-import axios from 'axios';
 import randomColor from 'randomcolor';
 import React, { useEffect, useState } from 'react';
 
 import HandleErrorMessages from '@/components/HandleErrorMessages';
-import Loading from '@/components/Loading';
 import NewQuoteButton from '@/components/NewQuoteButton';
 import QuoteDisplay from '@/components/QuoteDisplay';
 import {
   API_ERROR_MESSAGE,
   COLOR_LUMINOSITY,
-  LOCAL_STORAGE_QUOTES_KEY,
+  INITIAL_COLOR,
 } from '@/constants/index';
 import useErrorCountdown from '@/hooks/useErrorCountdown';
+import useQuote from '@/hooks/useQuote';
 
 import styles from '@/styles/index.module.css';
 
 export default function App() {
-  const [quoteBody, setQuoteBody] = useState(null);
-  const [color, setColor] = useState('black');
-  const [isLoading, setLoading] = useState(true);
-  const {
-    count,
-    isRunning,
-    errorMessage,
-    triggerSuccessCountdown,
-    triggerErrorCountdown,
-  } = useErrorCountdown();
+  const { data: quote, isFetching, isError, refetch } = useQuote();
+  const [color, setColor] = useState(INITIAL_COLOR);
+  const { count, isRunning } = useErrorCountdown(isFetching);
 
-  const getNewQuote = async () => {
-    try {
-      setColor(randomColor(COLOR_LUMINOSITY));
-      setLoading(true);
-      triggerSuccessCountdown();
-      const response = await axios.get('/api/proxy');
-      setQuoteBody(response.data[0]);
-    } catch (error) {
-      console.error('🚀 Error Message:', error);
-      triggerErrorCountdown(API_ERROR_MESSAGE);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Retrieve from localStorage and setQuoteBody if storedQuoteBody, otherwise requests new quoteBody.
   useEffect(() => {
-    // Make sure this code only runs on the client
-    if (typeof window !== 'undefined') {
-      const storedQuote = localStorage.getItem(LOCAL_STORAGE_QUOTES_KEY);
-
-      if (storedQuote) {
-        setColor(randomColor(COLOR_LUMINOSITY));
-        setQuoteBody(JSON.parse(storedQuote));
-        return;
-      }
-
-      getNewQuote();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // Set item (quoteBody) to localStorage if quoteBody.
-  useEffect(() => {
-    if (quoteBody) {
-      if (typeof window !== 'undefined') {
-        localStorage.setItem(
-          LOCAL_STORAGE_QUOTES_KEY,
-          JSON.stringify(quoteBody)
-        );
-        setLoading(false);
-      }
-    }
-  }, [quoteBody]);
+    setColor(randomColor(COLOR_LUMINOSITY));
+  }, [quote]);
 
   return (
     <div className={styles.container}>
-      <div className={styles.quoteBody}>
-        {isLoading && !quoteBody ? (
-          <Loading color={color} />
-        ) : (
-          <>
-            <h1 style={{ color }}>Random Quote Machine</h1>
-            <QuoteDisplay
-              quoteBody={quoteBody}
-              isLoading={isLoading}
-              color={color}
-            />
-            <NewQuoteButton
-              color={color}
-              count={count}
-              isRunning={isRunning}
-              errorMessage={!!errorMessage}
-              getNewQuote={getNewQuote}
-            />
-          </>
-        )}
-        {errorMessage && <HandleErrorMessages message={errorMessage} />}
+      <div className={styles.quoteContainer} style={{ color }}>
+        <h1>Quote Generator</h1>
+        <QuoteDisplay quoteData={quote} isFetching={isFetching} color={color} />
+        <NewQuoteButton
+          count={count}
+          isRunning={isRunning}
+          fetchQuote={refetch}
+        />
       </div>
+      {isError && isRunning && (
+        <HandleErrorMessages message={API_ERROR_MESSAGE} />
+      )}
     </div>
   );
 }
