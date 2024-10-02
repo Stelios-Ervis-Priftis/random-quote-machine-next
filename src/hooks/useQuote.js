86f5cca7c9be501e74088706e4fb72f03ded2499
Stelios Ervis Prifits <steliospriftis@gmail.com>
2024-10-02 15:39:41 +0200

    refactor(hooks & services): creating a utils folder for utility functions, rename file

--- src/hooks/useQuote.js
+++ src/hooks/useQuote.js
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { initialQuoteData, QUERY_QUOTE_KEY } from '@/constants/index';
-import fetchQuote from '@/services/quoteService';
+import fetchQuote from '@/services/utils/fetchQuote';
 
 const useQuote = () =>
   useQuery({
